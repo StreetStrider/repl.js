f74553177323bd3a263a9444a943c6cc09ae5d20
StreetStrider <teamfortresslife@gmail.com>
2013-12-10 01:44:46 +0400

    functools: signature.

--- src/functools.js
+++ src/functools.js
@@ -3,6 +3,8 @@
 
 module.exports = functools = {};
 
+var clc = require('cli-color');
+
 functools.expose = function (value, name)
 {
 	name || (name = '_');
@@ -12,4 +14,18 @@ functools.expose = function (value, name)
 functools.logAs = function (name)
 {
 	return console.log.bind(console, name);
+};
+
+var reFunction = /^(function(?:[^{]*){)/;
+
+functools.signature = function (fn)
+{
+	var
+		view  = fn.toString(),
+		match = reFunction.exec(view);
+
+	if (match)
+	{
+		console.log(clc.bold(match[1]) + ' ...');
+	}
 };
