c7bcae0a87cf9a7dd5da98cbfb01990591539b21
StreetStrider <teamfortresslife@gmail.com>
2015-04-08 23:26:23 +0300

    upgrade console with console-ultimate.

--- src/repl.js
+++ src/repl.js
@@ -1,18 +1,28 @@
 
 
-var std = require('repl');
-
 var repl = module.exports = {};
+
+var
+	std = require('repl');
+
+var
+	extend  = require('aux.js/object/extend'),
+	Console = require('console-ultimate');
 
 repl.start = function ()
 {
 	var instance = std.start({
-		prompt: 'js >',
+		prompt: 'js > ',
 		useGlobal: true,
 		ignoreUndefined: true
 	});
 
-	// console.log(instance.context == global);
+	var context = instance.context;
+
+	var output = instance.outputStream;
+	var uconsole = Console(output, output);
+
+	require('console-ultimate/global').replaceAt(context, uconsole);
 
 	return instance;
 }
