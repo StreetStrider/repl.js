a6efc403a91e843c9d4465fd2d565f780a844dfc
StreetStrider <teamfortresslife@gmail.com>
2015-05-22 22:58:23 +0300

    evaler: promise dynamic resolution with indicator.

--- src/evaler/evaler.js
+++ src/evaler/evaler.js
@@ -9,7 +9,8 @@ module.exports = function Evaler (options, console)
 {
 	var
 		vmEvaler,
-		noDisplay = { displayErrors: false };
+		noDisplay = { displayErrors: false },
+		slowThrs  = 350;
 
 
 	if (options.useGlobal)
@@ -48,18 +49,51 @@ module.exports = function Evaler (options, console)
 
 		new Promise(function (rs, rj)
 		{
+			enterPromise();
 			rs(vmEvaler(script, context));
 		})
 		.then(ok, error);
 
 		function ok (result)
 		{
+			leavePromise();
 			callback(null, result);
 		}
 		function error (error)
 		{
+			leavePromise();
 			console.trace(error);
 			callback();
+		}
+
+		var
+			slowIndicator,
+			isSlow;
+
+		var util = require('util');
+		var color = console.colors[util.inspect.styles.special];
+
+		function enterPromise ()
+		{
+			//console.info('enter promise');
+			slowIndicator = setTimeout(function ()
+			{
+				isSlow = true;
+				//console.warn('it isSlow');
+				console.writer.writeln('stdout', color('[Promise]'));
+			}, slowThrs);
+		}
+		function leavePromise ()
+		{
+			clearTimeout(slowIndicator);
+			//console.info('leave promise');
+			if (isSlow)
+			{
+				//console.warn('was isSlow = true');
+				//console.warn('_ must clear here');
+				console.writer.write('stdout', console.colors.move.up(1));
+				console.writer.write('stdout', console.colors.erase.line);
+			}
 		}
 	}
 
