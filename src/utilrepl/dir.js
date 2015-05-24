9b86013c058d2c1cd744dddf01b39844d97c065d
StreetStrider <teamfortresslife@gmail.com>
2015-05-24 17:39:27 +0300

    fix: global dir.

--- src/utilrepl/dir.js
+++ src/utilrepl/dir.js
@@ -5,16 +5,16 @@ module.exports = function (repl, console)
 {
 	var
 		context = repl.context,
-		dir     = console.dir;
+		_dir    = console.dir;
 
 	function dir (object)
 	{
 		if (! arguments.length)
 		{
-			object = context
+			object = context;
 		}
 
-		cdir(object, 0);
+		_dir(object, 0);
 
 		/* @todo: return value issue */
 		// return 'LOL'
